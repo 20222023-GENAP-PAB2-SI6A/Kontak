db3c773c58c97d40f5389d5f72ce29ddda5c1f31
Yohannes Masterous <yohannes.masterous@gmail.com>
2023-03-20 11:05:16 +0700

    Add user database

---
+++ app/src/main/java/masterous/si6a/kontak/db/UserDao.java
@@ -0,0 +1,24 @@
+package masterous.si6a.kontak.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+    @Insert
+    void insertUser(User user);
+
+    @Query("SELECT * FROM users")
+    List<User> getAllUsers();
+
+    @Update
+    int updateUser(User user);
+
+    @Query("DELETE FROM users WHERE id = :userId")
+    int deleteUser(int userId);
+}
