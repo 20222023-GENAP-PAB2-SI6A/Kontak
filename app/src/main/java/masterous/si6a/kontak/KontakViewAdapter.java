7eb3285046aa2432b6789c35838627629dd09d50
Yohannes Masterous <yohannes.masterous@gmail.com>
2023-03-27 10:14:33 +0700

    Update KontakViewAdapter.java

--- app/src/main/java/masterous/si6a/kontak/KontakViewAdapter.java
+++ app/src/main/java/masterous/si6a/kontak/KontakViewAdapter.java
@@ -1,5 +1,6 @@
 package masterous.si6a.kontak;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,20 +10,30 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+import masterous.si6a.kontak.databinding.ItemKontakBinding;
 import masterous.si6a.kontak.db.User;
 
 public class KontakViewAdapter extends RecyclerView.Adapter<KontakViewAdapter.ViewHolder> {
     private List<User> data = new ArrayList<>();
 
+    public void setData(List<User> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
     public KontakViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolder(ItemKontakBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull KontakViewAdapter.ViewHolder holder, int position) {
-
+        int pos = holder.getAdapterPosition();
+        User user = data.get(pos);
+        holder.itemKontakBinding.tvName.setText(user.getName());
+        holder.itemKontakBinding.tvEmail.setText(user.getEmail());
+        holder.itemKontakBinding.tvPhone.setText(user.getPhone());
     }
 
     @Override
@@ -31,9 +42,11 @@ public class KontakViewAdapter extends RecyclerView.Adapter<KontakViewAdapter.Vi
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private ItemKontakBinding itemKontakBinding;
                 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public ViewHolder(@NonNull ItemKontakBinding itemView) {
+            super(itemView.getRoot());
+            itemKontakBinding = itemView;
         }
     }
 }
